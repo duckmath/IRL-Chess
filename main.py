2187b897bae19ad91f4060990319a8d30fcf08c3
maddox05 <maddoxpublic@gmail.com>
2023-07-17 14:41:11 -0400

    i hate python

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#here
+import selenium
+from selenium import webdriver
+
+url = input("Enter the URL: ")
+print("The URL is: " + url)
+chrome_driver = webdriver.Chrome()
+chrome_driver.get(url)
+chrome_driver.quit()
+
+if __name__ == '__main__':
+    print(selenium.__version__)
