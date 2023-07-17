76273da05f8d1d417849394bdaf83ef45650098e
maddox05 <maddoxpublic@gmail.com>
2023-07-17 15:27:04 -0400

    chess.com test

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import selenium
-from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
 
-url = input("Enter the URL: ")
-print("The URL is: " + url)
-chrome_driver = webdriver.Chrome()
-chrome_driver.get(url)
-chrome_driver.quit()
-
+url = "https://www.chess.com/play/computer"#input("Enter the URL: ")
+#print("The URL is: " + url)
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+#elements = soup.find_all(class_="piece bn square-78")
+elements = soup.select('[class*="piece"]')
+for element in elements:
+    print(element)
 if __name__ == '__main__':
-    print(selenium.__version__)
+    print("hi")
