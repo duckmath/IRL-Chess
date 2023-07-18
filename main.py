6740b0231c0e4be70d0e07aad3a26c8f6967abf1
maddox05 <maddoxpublic@gmail.com>
2023-07-18 12:49:26 -0400

    sacrifice the rook!

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-example_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 url = "https://www.chess.com/game/9b5454d7-2584-11ee-a1b4-9d2e7001000f"
 login_url = "https://www.chess.com/login_check"
@@ -22,7 +21,15 @@ class Piece:
         self.color = color
 
     def __str__(self):
-        return f"{self.type} at {self.location} is {self.color}"
+        return f"{self.type} {self.location} is {self.color}"
+
+    class Location:
+        def __init__(self, col, row):
+            self.col = col
+            self.row = row
+
+        def __str__(self):
+            return f"col: {self.col}, row: {self.row}"
 
 
 # start a session
@@ -55,8 +62,10 @@ def live_loop():
             # print(element["class"])
             color = element["class"][1]
             location = element["class"][2].replace('square-', '')
-            item = Piece(color[1], location, color[0])
+            item = Piece(color[1], Piece.Location(int(location[0]) - 1, int(location[1]) - 1), color[0])
             print(str(item))
+            # how can I create a place to put these pieces?
+            # how can I make it so that it updates the board?
 
     return "Success"
 
